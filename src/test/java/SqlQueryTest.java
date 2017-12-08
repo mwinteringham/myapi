36863395338d91721a4db8b0ad740a4deb704ca8
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2017-12-08 19:33:18 +0000

    Test data for MySql now managed by test

--- src/test/java/SqlQueryTest.java
+++ src/test/java/SqlQueryTest.java
@@ -1,15 +1,36 @@
 import com.spun.util.io.FileUtils;
 import org.approvaltests.Approvals;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.myapi.MyAPIStartup;
 import org.myapi.QueryResult;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 
-
 public class SqlQueryTest{
+
+    @BeforeClass
+    public static void populateDB() throws SQLException {
+        String talksQuery  = "CREATE DATABASE talks;" +
+                             " USE talks; " +
+                             "CREATE TABLE talks ( title varchar(255) ); " +
+                             "INSERT INTO talks (title) VALUES ('COOL API DEMO'), ('My talk title');";
+
+        String sampleQuery = "USE talks;" +
+                             "CREATE TABLE sample ( id int, title varchar(255), money double, added datetime);" +
+                             "INSERT INTO sample VALUES (1, \"Title text 1\", 10.01, '2017-01-19 03:14:07.999999');" +
+                             "INSERT INTO sample VALUES (2, \"An interesting topic 2\", 12.31, '2017-11-11 03:14:07.999999');";
+
+        executeSqlQuery(talksQuery);
+        executeSqlQuery(sampleQuery);
+    }
 
     @Test
     public void testInsertIntoDB() throws IOException {
@@ -47,6 +68,11 @@ public class SqlQueryTest{
         verifyQuery("sample.json", "GET", "/sample", null);
     }
 
+    @AfterClass
+    public static void stripDB() throws SQLException {
+        executeSqlQuery("DROP DATABASE talks;");
+    }
+
     private void verifyQuery(String configFile, String httpMethod, String path, HashMap<String, String> params) throws IOException {
         MyAPIStartup myApi = new MyAPIStartup();
         myApi.loadJson(FileUtils.readFromClassPath(getClass(), "config.json"));
@@ -54,6 +80,12 @@ public class SqlQueryTest{
         QueryResult queryResult = myApi.call(httpMethod, path, params);
 
         Approvals.verify(queryResult.toString());
+    }
+
+    private static void executeSqlQuery(String sql) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306?allowMultiQueries=true", "root", "");
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.execute();
     }
 
 }
